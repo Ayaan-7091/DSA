a3171d7f5e25e70a051261d78ae576b1c9fdf4f2
Ayaan Shaikh <Samx7091@gmail.com>
2025-01-18 23:09:08 +0530

    Recursions sum-of-first-n-numbers optimal-approach

--- Python/Recursions/sum_of_n_numbers.py
+++ Python/Recursions/sum_of_n_numbers.py
@@ -6,4 +6,14 @@ def sum_n_numbers(n,sum):
         print(sum)
 
 n = int(input('Enter N : '))
-sum_n_numbers(n,0)
+# sum_n_numbers(n,0)
+
+# optimal approach
+
+def sum_n_numbers_op(n):
+    if(n==0):
+        return 0
+    else:
+        return n + sum_n_numbers_op(n-1)
+
+print(sum_n_numbers_op(n))
