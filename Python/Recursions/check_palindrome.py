6f3fc7d175ee7bc91f1afb82c3883028802d8572
Ayaan Shaikh <Samx7091@gmail.com>
2025-06-20 15:45:51 +0530

    check plaindrom - recursion - Optimal Soln

--- Python/Recursions/check_palindrome.py
+++ Python/Recursions/check_palindrome.py
@@ -1,16 +1,30 @@
+# string = "MADAM"
+# temp = ""
+# i = 0
+
+# def check(string,temp,i):
+#     if i == len(string):
+#         if string == temp:
+#             return "Palindrome"
+#         else:
+#             return "Not Palindrome"
+#     else :
+#         temp = temp + string[len(string)-1-i]
+#         return check(string,temp,i+1)
+
+# result = check(string,temp,i)
+# print(result)
+
+# OPTIMAL SOLUTION -->
+
+def is_palindrome(string, start, end):
+    if start >= end:
+        return "Palindrome"
+    
+    if string[start] != string[end]:
+        return "Not Palindrome"
+    
+    return is_palindrome(string, start+1, end - 1)
+
 string = "MADAM"
-temp = ""
-i = 0
-
-def check(string,temp,i):
-    if i == len(string):
-        if string == temp:
-            return "Palindrome"
-        else:
-            return "Not Palindrome"
-    else :
-        temp = temp + string[len(string)-1-i]
-        return check(string,temp,i+1)
-
-result = check(string,temp,i)
-print(result)
+print(is_palindrome(string, 0, len(string)-1))
