38f0c3e1dd4352b25dbe614cca4e458ce2ae5cfb
Ayaan Shaikh <Samx7091@gmail.com>
2025-06-20 16:13:33 +0530

    fibonacci - recursion - Optimal

--- Python/Recursions/fibonacci.py
+++ Python/Recursions/fibonacci.py
@@ -1,16 +1,27 @@
-n = 5
-n = n - 3 # to avoid the const 0 and 1
-i = 0
-f = [0,1]
+# n = 5
+# n = n - 3 # to avoid the const 0 and 1
+# i = 0
+# f = [0,1]
 
-def fib(n,i,f):
-    size = len(f)
-    if(i<=n):
-        f.append(f[size-1] + f[size-2])
-        return fib(n,i+1,f)
+# def fib(n,i,f):
+#     size = len(f)
+#     if(i<=n):
+#         f.append(f[size-1] + f[size-2])
+#         return fib(n,i+1,f)
     
-    return f
+#     return f
 
-print(fib(n,i,f))
+# print(fib(n,i,f))
 
 # OPTIMAL SOLUTION -->
+
+def fib(n, f=None):
+    if f is None:
+        f = [0,1]
+    
+    if len(f) >= n:
+        return f[:n]
+    f.append(f[-1] + f[-2])
+    return fib(n,f)
+n = 5
+print(fib(n))
