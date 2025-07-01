7467b0e108c79d7270c4bb17b3eb3d4323169cf6
Ayaan Shaikh <Samx7091@gmail.com>
2025-07-01 19:39:07 +0530

    Arrays - Find second largest Element - Optimal Approach

--- Python/Arrays/Easy/second_largest_element.py
+++ Python/Arrays/Easy/second_largest_element.py
@@ -16,3 +16,20 @@ def retrieve(array):
 array = [9,7,6,8]
 second_highest = retrieve(array)
 print(second_highest)
+
+#optimal approach
+def retrieve_op(array):
+    n = len(array)
+    large = float('-inf')
+    second_large = float('-inf')
+
+    for i in range(n):
+        if array[i] > large:
+            second_large = large 
+            large = array[i]
+        elif array[i] > second_large and array[i] != large :
+            second_large = array[i]
+    return second_large
+
+second_large = retrieve_op(array)
+print(second_large)
