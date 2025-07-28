2d073f3758648414c0e47bd8469c77a9a15c98e2
Ayaan Shaikh <Samx7091@gmail.com>
2025-07-29 00:08:34 +0530

    c++ : Pattern - 19 - corrected

--- C++/Patterns/19.cpp
+++ C++/Patterns/19.cpp
@@ -2,23 +2,29 @@
 using namespace std;
 int main(){
 
-    int n = 3;
+    int n = 6;
     for(int i=0;i<n;i++){
         for(int j=n;j>i;j--){
             cout << "*";
         }
         for(int k=0;k<2*i;k++){
-            cout << " /";
+            cout << " ";
+        }
+        for(int j=n;j>i;j--){
+            cout << "*";
         }
         cout << endl;
     }
 
-     for(int i=n;i>0;i--){
-        for(int j=n;j>=i;j--){
+     for(int i=n-1;i>=0;i--){
+        for(int j=n-1;j>=i;j--){
             cout << "*";
         }
-      for(int k=i;k<2*i;k++){
-            cout << " /";
+        for(int k=0;k<2*i;k++){
+            cout << " ";
+        }
+        for(int j=n-1;j>=i;j--){
+            cout << "*";
         }
         cout << endl;
     }
