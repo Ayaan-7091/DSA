e36dd8d0b5b291a9098478f819b7c3d9815dca55
Ayaan Shaikh <Samx7091@gmail.com>
2025-08-01 23:44:24 +0530

    c++ : Pattern - 22

--- C++/Patterns/22.cpp
+++ C++/Patterns/22.cpp
@@ -2,17 +2,12 @@
 using namespace std;
 int main(){
     int n = 4;
-    int temp = (n*2) - 1;
-    for(int i = 0;i<temp;i++){
-        for(int j = 0;j<((n*2)-1);j++){
-            if(i==0 || i==temp-1){
-              cout<<n<<" ";
-            }
-            else if(j==0||j==temp-1){
-                cout<<n<<" ";
-            }
-           
+    int size = (2*n)-1;
+  for(int i = 0; i < size; i++) {
+    for(int j = 0; j < size; j++) {
+        int min_dist = min(min(i, j), min(size - 1 - i, size - 1 - j));
+        cout << n - min_dist << " ";
     }
-    cout<<endl;
-    }
+    cout << endl;
+}
 }
