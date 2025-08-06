321a0517d70f198cf50a3c7a84f1ab546420e9ad
Ayaan Shaikh <Samx7091@gmail.com>
2025-08-07 00:01:30 +0530

    c++ : Number Problems - 1

--- C++/Number Problems/1.cpp
+++ C++/Number Problems/1.cpp
@@ -3,10 +3,8 @@ using namespace std;
 
 int main(){
     int n = 532012;
-    int temp;
     int count = 0;
     while(n!=0){
-        temp = n%10;
         count = count + 1;
         n = n/10;
     }
